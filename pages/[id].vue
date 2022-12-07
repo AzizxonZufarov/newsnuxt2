4aef8138463994f41e61f8eed62cad363a84ad2b
AzizxonZufarov <bionorica2015@mail.ru>
2022-12-07 11:47:48 +0500

    9

--- pages/[id].vue
+++ pages/[id].vue
@@ -10,18 +10,55 @@
     </button>
   </div>
 </template>
-<script setup>
-      definePageMeta({
-        layout: "stories"
-      })
-      const { id } = useRoute().params
+
+
+
+<script >
+
+  definePageMeta({
+    layout: "stories"
+  })
+export default {
+  
+  data () {
+    return {
+      uri: 'https://hacker-news.firebaseio.com/v0/item/'+ useRoute().params +'.json?print=pretty',
+      story: {},
+      err: '',
+      _story: {},
+      comments: [],
+    }
+  },
+  methods: {
+    async mounted() {
       
-      const uri = 'https://hacker-news.firebaseio.com/v0/item/'+ id +'.json?print=pretty'
 
-      const { data: story } = await useFetch(uri, { key:id }) 
-
-      if(!story.value) {
+      this.story = await useFetch(uri, { key: useRoute().params
+ })
+      console.log(this.story)
+      if(!this.story.value) {
         throw createError({ statusCode: 404, statusMessage: 'Story not found!', fatal: true })
       }
 
+        await $fetch('https://hacker-news.firebaseio.com/v0/'+ useRoute().params +'.json?print=pretty')
+          .then((response) => { 
+            this._story = this.response
+            this._story.comments = []
+
+            this._story.kids.forEach(id => {
+            $fetch('https://hacker-news.firebaseio.com/v0/item/'+ id +'.json?print=pretty')
+                .then((response) => {
+                    this.comments.push(response)
+                    console.log(this.comments)
+                })
+                .catch(err=> {
+                  this.err = err
+                })
+            })
+          })
+
+    }
+  }  
+}
 </script>
+
