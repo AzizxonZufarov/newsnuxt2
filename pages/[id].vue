01d978c234990213e6d1c18e29050d2f93c62067
AzizxonZufarov <bionorica2015@mail.ru>
2022-12-08 10:28:49 +0500

    11

--- pages/[id].vue
+++ pages/[id].vue
@@ -1,15 +1,24 @@
 <template>
   <div>
     <Head>
-    <Title>{{ story.title}}</Title>
+   <Title>{{ story.title}}</Title>
     <Meta name="description" content="product.description" /> 
   </Head>
-
-
     <StoryDetails :story="story" />
     <button class="btn">
         <NuxtLink to="/">Back to home</NuxtLink>
     </button>
+
+        <div class="card">
+        <div class="grid grid-cols-2 gap-5" v-for="comment in comments" :key="comment.id">
+                <div class="p-12">
+                <p class="text-4x1 my-7"><strong>Author: </strong>{{ comment.by }}</p>
+                <p class="text-4x1 my-7"><strong>Comments: </strong>{{ comment.text }}</p>
+                <p class="text-4x1 my-7"><strong>Date: </strong>{{ new Date(comment.time*1000).getDate()}}/{{ new Date(comment.time*1000).getMonth()+1}}/{{ new Date(comment.time*1000).getFullYear() }}</p>  
+
+            </div>
+        </div>
+    </div>
   </div>
 </template>
 
@@ -18,29 +27,40 @@
     layout: "stories"
   })
 
-	const { id } = useRoute().params
+ const { id } = useRoute().params
 
-  const uri = 'https://hacker-news.firebaseio.com/v0/item/'+ id +'.json?print=pretty'
+ const uri = 'https://hacker-news.firebaseio.com/v0/item/'+ id +'.json?print=pretty'
 
   const { data: story } = await useFetch(uri, { key:id })
 
-  const { data: comments} = []
+  const comments = []
+
+  const subcomments = []
+
+  const err = ''
+
+  story.value.kids.forEach(id => {
+    $fetch('https://hacker-news.firebaseio.com/v0/item/'+ id +'.json?print=pretty')
+        .then((response) => {
+
+            comments.push(response)
+            console.log(comments)
+
+            comments.kids.forEach(id => {
+              $fetch('https://hacker-news.firebaseio.com/v0/item/'+ id +'.json?print=pretty')
+                  .then((response) => {
+                      subcomments.push(response)
+                      console.log(subcomments)
+                  })
+            })
+        })
+        .catch(err=> {
+          err.value = err
+        })
+  })
+  
 
 
 
-  if(!this.story.value) {
-    throw createError({ statusCode: 404, statusMessage: 'Story not found!', fatal: true })
-  }
 
-    this.story.kids.forEach(id => {
-      $fetch('https://hacker-news.firebaseio.com/v0/item/'+ id +'.json?print=pretty')
-          .then((response) => {
-              this.comments.push(response)
-              console.log(this.comments)
-          })
-          .catch(err=> {q
-            this.err = err
-          })
-      })
-  console.log(this.comments)
 </script>
